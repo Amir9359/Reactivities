4ac075cb55bbc02f8d7222047e74d0231e128429
Amir9359 <52462695+Amir9359@users.noreply.github.com>
2022-04-17 12:58:51 +0430

    End of Sec 6 test

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -16,7 +16,6 @@ namespace Application.Activities
         public class Handler : IRequestHandler<Query, List<Activity>>
         {
             private readonly DataContext _context;
-            private readonly ILogger<List> _logger;
             public Handler(DataContext context)
             {
                 _context = context;
